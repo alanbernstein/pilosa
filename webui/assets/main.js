aad6f2deedae2b77d87344c6646f973f5a334c74
Linh Vo <lvo@umbel.com>
2017-04-27 12:06:47 -0500

    fix markup string

--- webui/assets/main.js
+++ webui/assets/main.js
@@ -11,9 +11,24 @@ class REPL {
     bind_events() {
         const repl = this
         const keys = {
+          TAB: 9,
           ENTER: 13,
           UP_ARROW: 38,
           DOWN_ARROW: 40
+        }
+        const keywords = {
+          // keyword: length of substring that comes after cursor
+          "SetBit()": 1,
+          "ClearBit()": 1,
+          "SetBitmapAttrs()": 1,
+          "Bitmap()": 1,
+          "Union()": 1,
+          "Intersect()": 1,
+          "Difference()": 1,
+          "Count()": 1,
+          "Range()": 1,
+          "TopN()": 1,
+          "frame=": 0,
         }
 
         this.input.addEventListener("keydown", (e) => {
@@ -52,6 +67,33 @@ class REPL {
             e.preventDefault()
             this.submit();
           }
+          if (e.keyCode == keys.TAB) {
+            e.preventDefault()
+
+            // extract word ending at cursor
+            var word_start = this.input.selectionEnd-1
+            while(word_start>0) {
+              var c = this.input.value.charCodeAt(word_start)
+              if(!((c>64 && c<91) || (c>96 && c<123))) {
+                word_start++
+                break
+              }
+              word_start--
+            }
+            var input_word = this.input.value.substring(word_start, this.input.selectionEnd)
+
+            // check for keyword match and insert
+            // this just stops at the first match
+            for(var keyword in keywords) {
+              if(keyword.startsWith(input_word)){
+                var completion = keyword.substring(input_word.length)
+                this.input.value += completion
+                var pos = this.input.value.length - keywords[keyword]
+                this.input.setSelectionRange(pos, pos)
+                break
+              }
+            }
+          }
         })
         this.button.onclick = function() {
             repl.submit();
@@ -69,42 +111,72 @@ class REPL {
 
     process_query(query) {
         var xhr = new XMLHttpRequest();
+        var url, data;
         var e = document.getElementById("index-dropdown");
         var indexname = e.options[e.selectedIndex].text;
-        xhr.open('POST', '/index/' + indexname + '/query');
+        if (query.startsWith(":")) {
+            var keys = query.replace(/\s+/g, " ").split(" ");
+            var command = keys[0];
+            var command_type = keys[1];
+            var command_name = keys[2];
+            switch (command) {
+                case ":create":
+                    switch (command_type) {
+                        case "index":
+                            url = '/index/' + command_name;
+                            data = "";
+                            break;
+                        case "frame":
+                            if (!command_name) {
+                                command_name = "";
+                            }
+                            url = '/index/' + indexname + '/frame/' + command_name;
+                            data = "";
+                            break;
+                    }
+            }
+        } else {
+            url = '/index/' + indexname + '/query';
+            data = query
+        }
+        xhr.open('POST', url);
         xhr.setRequestHeader('Content-Type', 'application/text');
 
-        const repl = this
+        const repl = this;
         var start_time = new Date().getTime();
-        xhr.send(query)
-        xhr.onload = function() {
-            var end_time = new Date().getTime()
+        xhr.onload = function () {
+            var end_time = new Date().getTime();
             repl.result_number++
             repl.createSingleOutput({
-              "input": query, 
-              "output": xhr.responseText, 
-              "indexname": indexname,
-              "querytime_ms": end_time - start_time,
+                "input": query,
+                "output": xhr.responseText,
+                "status": xhr.status,
+                "indexname": indexname,
+                "querytime_ms": end_time - start_time,
             })
-        }
+        };
 
+        xhr.send(data);
     }
 
     createSingleOutput(res) {
       var node = document.createElement("div");
       node.classList.add('output');
       var output_string = res['output']
-      var output_json = JSON.parse(output_string)
-      var result_class = "result-output"
+      var  result_class = "result-output"
       var getting_started_errors = [
         'index not found',
         'frame not found',
       ]
-
-      if("error" in output_json) {
-        result_class = "result-error"
-        if(getting_started_errors.indexOf(output_json['error']) >= 0) {  
-          output_string += `<br />
+        var output_json;
+      // handle output formatting
+        if(res["status"] != 200) {
+            result_class = "result-error";
+            if (isJSON(output_string)) {
+                output_json = JSON.parse(output_string)
+                if ("error" in output_json) {
+                    if (getting_started_errors.indexOf(output_json['error']) >= 0) {
+                        output_string += `<br />
           <br />
           Just getting started? Try this:<br />
           $ curl -XPOST "http://127.0.0.1:10101/index/test" -d '{"options": {"columnLabel": "col"}}' # create index "test"<br />
@@ -112,11 +184,41 @@ class REPL {
           # Select "test" in the index dropdown above<br />
           SetBit(row=0, col=0, frame=foo) # Use PQL to set a bit
           `
+                    }
+                }
+            }
         }
+
+
+      if(res["input"].startsWith("TopN(")) {
+        // this assumes only one PQL command was included in the query.
+        // any elements of results after the first are ignored
+        result_class = "result-table"
+
+      var topn_list = output_json["results"][0]
+        var table = document.createElement("table")
+
+        var header = document.createElement('tr')
+        markup = `<th>id</th>
+        <th>count</th>`
+        header.innerHTML = markup
+        table.appendChild(header)
+        for(var n=0; n<topn_list.length; n++) {
+          var row = document.createElement("tr")
+          markup = `<td>${topn_list[n]["id"]}</td>
+          <td>${topn_list[n]["count"]}</td>`
+          row.innerHTML = markup
+          table.appendChild(row)
+        }
+
+        // yuck
+        var temp_div = document.createElement("div")
+        temp_div.appendChild(table)
+        output_string = temp_div.innerHTML
       }
 
-
-      var markup =`
+      // create elements and add to page
+        var markup =`
         <div  class="panes">
           <div class="pane active">
             <div class="result-io">
@@ -148,49 +250,65 @@ class REPL {
           </div>
         </div>
       `
-      node.innerHTML = markup;
-      this.output.insertBefore(node, this.output.firstChild)
+        node.innerHTML = markup;
+        this.output.insertBefore(node, this.output.firstChild)
     }
 
     populate_index_dropdown() {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', '/schema')
-      var select = document.getElementById('index-dropdown')
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/schema')
+        var select = document.getElementById('index-dropdown')
 
-      xhr.onload = function() {
-        var schema = JSON.parse(xhr.responseText)
-        for(var i=0; i<schema['indexes'].length; i++) {
-          var opt = document.createElement('option')
-          opt.value = i+1
-          opt.innerHTML = schema['indexes'][i]['name']
-          select.appendChild(opt)
+        xhr.onload = function () {
+            var schema = JSON.parse(xhr.responseText)
+            for (var i = 0; i < schema['indexes'].length; i++) {
+                var opt = document.createElement('option')
+                opt.value = i + 1
+                opt.innerHTML = schema['indexes'][i]['name']
+                select.appendChild(opt)
+            }
+            // set the active option to one of the populated options, instead of invalid "Index"
+            if (i > 0) {
+                select.value = 1;
+            }
         }
-        // set the active option to one of the populated options, instead of invalid "Index"
-        if(i > 0) {
-          select.value = 1;
-        }
-      }
-      xhr.send(null)
+        xhr.send(null)
     }
 
 }
 
-function setNav(e) {
-  // toggle the nav buttons
-  document.getElementsByClassName("nav-active")[0].classList.remove("nav-active")
-  e.classList.add("nav-active")
+function populate_version() {
+  var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/version')
+    var node = document.getElementById('server-version')
 
-  // toggle the main interface content divs
-  document.getElementsByClassName("interface-active")[0].classList.remove("interface-active")
-  name = e.id.substring(4)
-  interface_el = document.getElementById('interface-' + name)
-  interface_el.classList.add("interface-active")
+    xhr.onload = function() {
+      version = JSON.parse(xhr.responseText)['version']
+      node.innerHTML = version
+    }
+    xhr.send(null)
+}
+
+function setNav(e) {
+    // toggle the nav buttons
+    document.getElementsByClassName("nav-active")[0].classList.remove("nav-active")
+    e.classList.add("nav-active")
+
+    // toggle the main interface content divs
+    document.getElementsByClassName("interface-active")[0].classList.remove("interface-active")
+    name = e.id.substring(4)
+    interface_el = document.getElementById('interface-' + name)
+    interface_el.classList.add("interface-active")
 
   // hack hack
-  if(name == "cluster") {
+  switch(name ) {
+    case "cluster":
     update_cluster_status()
-  }
-
+  break
+    case "documentation":
+      open_external_docs()
+      break
+}
 }
 
 
@@ -200,41 +318,115 @@ function update_cluster_status() {
   status_node = document.getElementById('status')
   time_node = document.getElementById('status-time')
   xhr.onload = function() {
-    status_formatted = JSON.stringify(JSON.parse(xhr.responseText), null, 4)
-    status_node.innerHTML = highlightJSON(status_formatted)
+    var status =JSON.parse(xhr.responseText)
+    render_status(status)
     time_node.innerHTML = new Date().today() + " " + new Date().timeNow()
   }
   xhr.send(null)
 }
 
-Date.prototype.today = function () { 
-    return this.getFullYear() +"/"+ (((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ ((this.getDate() < 10)?"0":"") + this.getDate();
+function render_status(status) {
+  // render node table
+  var nodes_div = document.getElementById("status-nodes")
+  while (nodes_div.firstChild) {
+    nodes_div.removeChild(nodes_div.firstChild);
+  }
+
+  var nodes = status["status"]["Nodes"]
+  table = document.createElement("table")
+  tbody = document.createElement("tbody")
+  table.appendChild(tbody)
+  var caption = document.createElement("caption")
+  caption.innerHTML = "(" + nodes.length + ")"
+  tbody.appendChild(caption)
+
+  var header = document.createElement('tr')
+  markup = `<th>Host</th>
+  <th>State</th>`
+  header.innerHTML = markup
+  table.appendChild(header)
+  for(var n=0; n<nodes.length; n++) {
+    var row = document.createElement("tr")
+    markup = `<td>${nodes[n]["Host"]}</td>
+    <td>${nodes[n]["State"]}</td>`
+    row.innerHTML = markup
+    tbody.appendChild(row)
+  }
+  nodes_div.appendChild(table)
+
+  // render index tables
+  var indexes_div = document.getElementById("status-indexes")
+  while (indexes_div.firstChild) {
+    indexes_div.removeChild(indexes_div.firstChild);
+  }
+
+  var indexes = nodes[0]["Indexes"] // TODO currently comes from only node 0
+  for(var n=0; n<indexes.length; n++) {
+    table = document.createElement("table")
+    tbody = document.createElement("tbody")
+    table.appendChild(tbody)
+    var caption = document.createElement("caption")
+    caption.innerHTML = indexes[n]["Name"] + " (Column Label: " + indexes[n]["Meta"]["ColumnLabel"] + ")"
+    table.appendChild(caption)
+
+    var header = document.createElement('tr')
+    markup = `<th>Name</th>
+    <th>Row Label</th>
+    <th>Cache Type</th>
+    <th>Cache Size</th>`
+    header.innerHTML = markup
+    tbody.appendChild(header)
+
+    var frames = indexes[n]["Frames"]
+    for(var m=0; m<frames.length; m++) {
+      var row = document.createElement("tr")
+      markup = `<td>${frames[m]["Name"]}</td>
+      <td>${frames[m]["Meta"]["RowLabel"]}</td>
+      <td>${frames[m]["Meta"]["CacheType"]}</td>
+      <td>${frames[m]["Meta"]["CacheSize"]}</td>`
+      row.innerHTML = markup
+      tbody.appendChild(row)
+    }
+    indexes_div.appendChild(table)
+  }
+
+  // render slice tables
+  // TODO enable when Slices element is present in status response
+  /*
+  var slices_div = document.getElementById("status-slices")
+  data = ""
+  for(var n=0; n<nodes.length; n++) {
+    var indexes = nodes[n]["Indexes"]
+    for(var m=0; m<indexes.length; m++) {
+      data += nodes[n]["Host"] + "/" + indexes[m]["Name"] + ": " + indexes[m]["Slices"] + "<br />"
+    }
+  }
+  slices_div.innerHTML = data
+  */
+
+}
+
+function open_external_docs() {
+  window.open("https://www.pilosa.com/docs");
+}
+
+Date.prototype.today = function () {
+    return this.getFullYear() + "/" + (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + ((this.getDate() < 10) ? "0" : "") + this.getDate();
 }
 
 Date.prototype.timeNow = function () {
-     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
 }
 
-function highlightJSON(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
+populate_version()
+function isJSON(str) {
+    try {
+        JSON.parse(output_string)
+    } catch (e) {
+        return false
+    }
+    return true
 }
-
-
 const input = document.getElementById('query')
 const output = document.getElementById('outputs')
 const button = document.getElementById('query-btn')
